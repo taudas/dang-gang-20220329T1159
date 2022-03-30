77114c53f1e887fa8db6cbffbdcc755c5c6a069e
taudas <taudas@gmail.com>
2022-03-30 13:53:44 -0400

    second try Html

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {
 "assets/NOTICES": "eddb6adaf40a8d107d5d4fcf35d16f1e",
 "assets/AssetManifest.json": "e9760aff26d7236650b16d3f72345665",
 "assets/FontManifest.json": "d751713988987e9331980363e24189ce",
-"main.dart.js": "a894934f2562d06a317f6350eca6c0ab",
+"main.dart.js": "8859d58c88a67b8a64a6903ecdd2fb8f",
 "version.json": "cfdc42de594f8b1cdbc3cfd2b33da3ea",
 "index.html": "51839f812a32cce7ac40ca488295da30",
 "/": "51839f812a32cce7ac40ca488295da30",
